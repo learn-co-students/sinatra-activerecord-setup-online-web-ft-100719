b1cff728be223fe9463671ab9f855aa9def53ab9
Huda Yousif <hjoski@gmail.com>
2019-11-20 12:45:30 -0800

    Done.

--- db/migrate/20191120204031_create_dogs.rb
+++ db/migrate/20191120204031_create_dogs.rb
@@ -11,5 +11,15 @@ class CreateDogs < ActiveRecord::Migration[5.2]
     drop_table :dogs
   end 
 
+# The change method is actually a shorter way of writing up and down methods. 
+# We can refactor our migration to look like this:
+
+# def change
+#   create_table :dogs do |t|
+#     t.string :name
+#     t.string :breed
+#   end
+# end
+
 end
 
